9ea068a8b22aefa2dde163bb52ba6724f8af3d83
Shchepetilnikov <kalugalina.gugl@gmail.com>
2022-07-13 20:29:55 +0300

    added_homework_4

---
+++ Program_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int SumOfDigit(int number)
+{
+    int sum = 0;
+    for(int i = 10; number / i > 0; number = number / i)
+    {
+        sum += number % i;
+    }
+    sum += number % 10;
+    return sum;
+}
+
+Console.WriteLine("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = SumOfDigit(num);
+
+Console.WriteLine("Sum of digit is: " + result);
---
+++ Program_2/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] CreateArray(int min, int max)
+{
+    int[] array = new int[8];
+
+    for(int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < 8; i++)
+        Console.WriteLine(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("input minimal value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input maximum value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int[] array = CreateArray(min, max);
+
+ShowArray(array);
